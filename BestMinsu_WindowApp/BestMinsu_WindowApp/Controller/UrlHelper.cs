698bae524ea2df069f90ee217e452fc91d8ed08e
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2022-04-09 16:50:50 +0900

    Add WindowApp

---
+++ BestMinsu_WindowApp/BestMinsu_WindowApp/Controller/UrlHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using OpenQA.Selenium;
+
+public static class UrlHelper {
+    public enum Target {
+        Main,
+        TransactionInfo,
+        News,
+        Analysis
+    }
+
+    static string ToUrl(this Target target) {
+        return target switch {
+            Target.Main => "main",
+            Target.TransactionInfo => "frgn",
+            Target.News => "news",
+            Target.Analysis => "coinfo",
+            _ => ""
+        };
+    }
+
+    public static string GetUrl(int code, Target target = Target.Main) {
+        string url = $"https://finance.naver.com/item/{target.ToUrl()}.naver?code={code:000000}";
+        return url;
+    }
+
+    public static void GoTo(this IWebDriver driver, int code) {
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+        
+        // 해당 Url로 이동한다.
+        driver.Url = GetUrl(code);
+        Console.WriteLine($"URL: {driver.Url}");
+        
+        // Url 로딩 후에는 페이지의 요소를 찾는 시간을 제거한다.
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+    }
+}
---
+++ BestMinsu_WindowApp/BestMinsu_WindowApp/Utility/CustomExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+public static class CustomExtension {
+    public static int? ToInt32(this string numeric) {
+        if (int.TryParse(numeric, NumberStyles.Any, CultureInfo.InvariantCulture, out var ret)) {
+            return ret;
+        }
+
+        return null;
+    }
+    
+    public static long? ToInt64(this string numeric) {
+        if (long.TryParse(numeric, NumberStyles.Any, CultureInfo.InvariantCulture, out var ret)) {
+            return ret;
+        }
+
+        return null;
+    }
+
+    public static int ToInt32(this string numeric, int defaultValue)
+        => numeric.ToInt32() ?? defaultValue;
+
+    public static float? ToFloat(this string floatValue) {
+        if (float.TryParse(floatValue, NumberStyles.Any, CultureInfo.InvariantCulture, out var ret)) {
+            return ret;
+        }
+
+        return null;
+    }
+    
+    public static float ToFloat(this string floatValue, float defaultValue)
+        => floatValue.ToFloat() ?? defaultValue;
+    
+    public static float CalculateDiffRate(this float origin, float target, float coefficient = 1f, float max = float.MaxValue, float min = 1f) {
+        if (origin == 0f || target == 0f) return 0f;
+
+        float diffRate = (origin > target ? origin / target : target / origin) - 1f;
+        return (diffRate * coefficient).Clamp(min, max);
+    }
+
+    public static float CalculateDiffRate(this float? origin, float? target, float coefficient = 1f, float max = float.MaxValue, float min = 1f) {
+        if (origin == null || target == null) return 0f;
+        return ((float)origin).CalculateDiffRate((float)target, coefficient, max, min);
+    }
+
+    public static float Clamp(this float origin, float min, float max) {
+        if (origin < min) return min;
+        if (origin > max) return max;
+        return origin;
+    }
+    
+    public static int Clamp(this int origin, int min, int max) {
+        if (origin < min) return min;
+        if (origin > max) return max;
+        return origin;
+    }
+}
